fa9eb6ee60397af7fae989c05d2edef1b8729493
lexoyo <a.hoyau@silexlabs.org>
2014-09-02 22:05:47 +0200

    first commit

---
+++ js/jquery.flickr.js
@@ -0,0 +1,102 @@
+﻿/*
+ * jQuery Flickr - jQuery plug-in
+ * Beta 1, Released 2008.02.26
+ * https://code.google.com/p/jquery-flickr/source/browse/#svn%2Ftrunk
+ *
+ * Copyright (c) 2008 Daniel MacDonald (www.projectatomic.com)
+ * Dual licensed GPL http://www.gnu.org/licenses/gpl.html
+ * and MIT http://www.opensource.org/licenses/mit-license.php
+ */
+(function($) {
+$.fn.flickr = function(o){
+    var s = {
+        api_url: null,
+        callback: null,
+        error: null,    // error callback
+        api_key: null,
+        type: null,         // allowed values: 'photoset', 'search'
+        photoset_id: null,
+        user_id: null,
+        group_id: null,
+        tags: null,         // comma separated list
+        tag_mode: null,     // allowed values: 'any' (OR), 'all' (AND)
+        text: null,			// free text search
+        sort: null,         // date-posted-asc, date-posted-desc, date-taken-asc, date-taken-desc, interestingness-desc, interestingness-asc, relevance
+        thumb_size: 's',    // allowed values: s (75x75), t (100x?), m (240x?)
+        size: null,         // default: (500x?), allowed values: m (240x?),  o (original)
+        per_page: null,     // default: 100, max: 500
+        page: null,     	// default: 1
+        displayType: null  //allowed values: litebox, slideshow, gallery - default: gallery
+    };
+    if(o) $.extend(s, o);
+    return this.each(function(){
+    var container = this;
+		var list = $('<ul>');
+        var url = $.flickr.format(s);
+    $.getJSON(url, function(r){
+          if (r.stat != "ok"){
+              if (s.error){
+                  s.error('flickr error: '+ r.message);
+              }
+	        } else {
+	            if (s.type == 'photoset'){
+	                r.photos = r.photoset;
+	            }
+	            for (var i=0; i<r.photos.photo.length; i++){
+	                var photo = r.photos.photo[i];
+	                var t = 'http://farm'+photo['farm']+'.static.flickr.com/'+photo['server']+'/'+photo['id']+'_'+photo['secret']+'_'+s.thumb_size+'.jpg';
+	                var h = 'http://farm'+photo['farm']+'.static.flickr.com/'+photo['server']+'/'+photo['id']+'_';
+	                if (!s.size) h += photo['secret']+'.jpg';
+	                else if (s.size == 'o') h += photo['originalsecret']+'_o.'+photo['originalformat'];
+	                else h += photo['secret']+'_'+s.size+'.jpg';
+                  if (s.displayType === 'slideshow'){
+                    list.append('<li style="background-image: url('+h+')" alt="'+photo['title']+'" /></li>');
+                  }
+                  else{
+                    list.append('<li><a href="'+h+'"><img src="'+t+'" alt="'+photo['title']+'" /></a></li>');
+                  }
+              }
+              if(s.displayType === 'litebox'){
+                list.litebox();
+              }
+              if (s.callback){
+                $(container).html('');
+                list.appendTo(container);
+                s.callback();
+              }
+          }
+		}).error(function() {
+        if (s.error){
+            s.error('network error');
+        }
+    });
+    });
+};
+$.flickr = {
+    format: function(s){
+        if (s.url) return s.url;
+        var url = s.api_url || 'https://api.flickr.com/';
+        url += 'services/rest/?format=json&jsoncallback=?&api_key='+s.api_key;
+        switch (s.type){
+            case 'photoset':
+                url += '&method=flickr.photosets.getPhotos&photoset_id=' + s.photoset_id;
+                break;
+            case 'search':
+                url += '&method=flickr.photos.search';
+                if (s.user_id) url += '&user_id=' + s.user_id;
+                if (s.group_id) url += '&group_id=' + s.group_id;
+                if (s.tags) url += '&tags=' + s.tags;
+                if (s.tag_mode) url += '&tag_mode=' + s.tag_mode;
+                if (s.text) url += '&text=' + s.text;
+                if (s.sort) url += '&sort=' + s.sort;
+                break;
+            default:
+                url += '&method=flickr.photos.getRecent';
+        }
+        if (s.per_page) url += '&per_page=' + s.per_page;
+        if (s.page) url += '&page=' + s.page;
+        if (s.size == 'o') url += '&extras=original_format';
+        return url;
+    }
+};
+})(jQuery);
---
+++ js/script.js
@@ -0,0 +1,64 @@
+/*
+var albums = {
+    home: '72157608376528102',
+    abstract: '72157624708853721',
+    paysages: '72157622949851126',
+    portraits: '72157605117901038',
+    default: '',
+}
+*/
+
+document.write('<script type="text/javascript" src="js/jquery.flickr.js"></scr'+'ipt>');
+
+function flickIt(jQuerySelector, displayType, photosetId, opt_cbk){
+    jQuerySelector.html('loading...');
+    jQuerySelector.flickr({
+        api_key: "7b5f9448ae4547ff02d6ce3db22e482d",
+        type: 'photoset',
+        photoset_id: photosetId,
+        size: 'o',
+        thumb_size: 'm',
+        displayType: displayType,
+        callback: function(){
+            if (opt_cbk){
+                opt_cbk(jQuerySelector);
+            }
+        },
+        error: function(e){
+            jQuerySelector.html('Error loading flickr gallery. ' + e);
+        }
+    });
+}
+
+function slideIt(jQuerySelector){
+    $(jQuerySelector).find('ul > li:gt(0)').css('opacity', '0');
+    setInterval(function() {
+      $(jQuerySelector).find('ul > li:first')
+        .fadeOut(1000)
+        .next()
+        .css('opacity', '1')
+        .fadeIn(1000)
+        .end()
+        .appendTo($(jQuerySelector).find('ul'));
+    },  10000);
+}
+function getPhotosetId(pageName){
+    return pageName;
+    //return albums[pageName] || albums['default'];
+}
+function onPageChange(newUrl){
+    var currentPage = newUrl.substr(newUrl.indexOf('#!page-') + 7);
+    window.jQuerySelector = $(".gallery");
+    flickIt(jQuerySelector, 'gallery', getPhotosetId(currentPage));
+}
+$(function(){
+    window.jQuerySelector = $(".flickr .silex-element-content");
+    //flickIt(jQuerySelector, 'slideshow', albums.home, slideIt);
+    var currentPage = $('body').pageable().data().silexlabsPageable.options.currentPage
+    currentPage = currentPage.substr(currentPage.indexOf('page-') + 5);
+    flickIt(jQuerySelector, 'slideshow', currentPage, slideIt);
+    onPageChange(window.location.href);
+})
+$(window).bind( 'hashchange', function (e){
+    onPageChange(e.originalEvent.newURL);
+});
